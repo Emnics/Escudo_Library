301accd519bd5906068b9823a0120b8618540083
Emnics <divya@emnics.com>
2015-12-10 14:19:50 +0530

    Create Escudo_eeprom.py

---
+++ Escudo_eeprom.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import os
+import smbus
+import thread
+bus=smbus.SMBus(1)
+GPIO.setmode(GPIO.BOARD)
+
+
+eeprom_I2C_ID=0x50              #eeprom I2C address
+data= 10                        # data to be written
+eeprom_reg_address=0
+bus.write_byte_data(eeprom_I2C_ID,eeprom_reg_address,data)   
+
+sleep(0.7)
+
+while(1):
+
+   read_data=bus.read_byte_data(eeprom_I2C_ID,eeprom_reg_address)
+   if(read_data==data):
+       print "EEPROM WORKING"
