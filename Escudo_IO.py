4ac70416239d68f3ba169fb18d923f053288b47c
Emnics <divya@emnics.com>
2016-03-09 10:37:51 +0530

    Update Escudo_IO.py

--- Escudo_IO.py
+++ Escudo_IO.py
@@ -4,22 +4,25 @@ Organization:  Emnics Technologies Pvt Ltd
 Description :  Sample code for all the available GPIOs in ESCUDO
 ***********************************************************************"""
 
+import RPi.GPIO as GPIO
+from time import sleep
+import os
+import smbus
+import thread
+bus=smbus.SMBus(1)                     #I2C Library 
+GPIO.setmode(GPIO.BOARD)               #Pin No Selection based on Board
 
-import RPi.GPIO as GPIO              # Importing RPi.GPIO library
-from time import sleep               # Importing Delay Library
-GPIO.setmode(GPIO.BOARD)             # seting GPIO Pin number selection based on board Pin no
+GPIO.setwarnings(False)
 
-GPIO.setwarnings(False)              # Set the warning false to reuse the GPIO
+"""****************Pin Assignment************************************"""
+RELAY = [7,11,12,13,15,16,18,24]       #Relay Pin Nos
+INPUT =[40,37,38,35,36,33,31,32]       #Input Pin Nos
+LED = [29]                             #LED Pin No
+BUZZER = [22]                          #Buzzer Pin No
+"""******************************************************************"""
 
-RELAY = [7,11,12,13,15,16,18,24]     # Pin nos of the relays in ESCUDO connected to Raspberry Pi
 
-LED = [29]                           # On board LED Pin No
-
-BUZZER = [22]                        #Escudo Buzzer Pin No
-
-INPUTS=[40,37,38,35,36,33,31,32]
-
-#----set the direction of Relays, Buzzer and LED as output-----#
+"""*****************Pin Direction (Output/Input)**********************"""
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
@@ -27,13 +30,10 @@ GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
 GPIO.setup(16,GPIO.OUT)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 GPIO.setup(18,GPIO.OUT)
-GPIO.setup(22,GPIO.OUT)
 GPIO.setup(24,GPIO.OUT)
 GPIO.setup(29,GPIO.OUT)
-#---------------------------------------------------------------#
+GPIO.setup(22,GPIO.OUT)
 
-
-#--set the direction of ESCUDO Opto Isolated input pin as Input-#
 GPIO.setup(40,GPIO.IN)
 GPIO.setup(37,GPIO.IN)
 GPIO.setup(38,GPIO.IN)
@@ -42,31 +42,138 @@ GPIO.setup(36,GPIO.IN)
 GPIO.setup(33,GPIO.IN)
 GPIO.setup(31,GPIO.IN)
 GPIO.setup(32,GPIO.IN)
-#----------------------------------------------------------------#
+"""*********************************************************************"""
 
-#-------------Set all outputs to Low/False-----------------------#
+"""************************SPI Pin Assignment**************************"""
+SPICLK=23
+SPIMOSI=19
+SPIMISO=21
+SPICS=26
 
-#GPIO.output(BUZZER[0],False)
+GPIO.setup(SPIMOSI,GPIO.OUT)
+GPIO.setup(SPIMISO,GPIO.IN)
+GPIO.setup(SPICLK,GPIO.OUT)
+GPIO.setup(SPICS,GPIO.OUT)
+"""*********************************************************************"""
 
-GPIO.output(RELAY[0],False)
-GPIO.output(RELAY[1],False)
-GPIO.output(RELAY[2],False)
-GPIO.output(RELAY[3],False)
-GPIO.output(RELAY[4],False)
-GPIO.output(RELAY[5],False)
-GPIO.output(RELAY[6],False)
-GPIO.output(RELAY[7],False)
-#------------------------------------------------------------------#
+GPIO.output(BUZZER[0],False )            # Buzzer Turned OFF by default
+address=0x68                             # RTC Register Address                  
+address1=0x50                            # EEPROM Register Address
 
-#---------------Entering while loop---------------------------------#
-while(1):
 
-    #----monitoring GPIO input pin continously------------#
-    if(GPIO.input(INPUTS[0])==0):
-       GPIO.output(RELAY[7],True)        # setting Relay 8(RELAY[7] true if Input 1 is low
-       print "sw on"
+
+#""*********************************************************************"""
+#Function Name   :Turn_ON_Buzzer
+#Descriptions    :Turns ON Buzzer, Buzzer PIN becomes high
+#Argument        :NA
+#Return          :NA
+"**************************************************************************"
+def Turn_ON_Buzzer():
+    GPIO.output(BUZZER[0],True)
+
+#"""********************************************************************""""
+
+
+
+#""*********************************************************************"""
+#Function Name   :Turn_OFF_Buzzer
+#Descriptions    :Turns OFF Buzzer, Buzzer PIN becomes LOW
+#Argument        :NA
+#Return          :NA
+"*********************************************************************"""
+
+def Turn_OFF_Buzzer():
+    GPIO.output(BUZZER[0],False)
+
+#""********************************************************************""""
+
+#"""""*********************************************************************"""
+#Function Name   :Turn_ON_Relay(relay_no)
+#Descriptions    :Turns ON corresponding Relays. Relay Pin goes high
+#Argument        :relay_no (Relay Number between 1 to 8)
+#Return          :NA
+"***********************************************************************"""
+
+def Turn_ON_Relay(relay_no):
+    relay_no=relay_no-1
+    if((relay_no>7)or (relay_no<0)):
+        print("please rnter correct relay noumber between 1 to 8")
     else:
-        GPIO.output(RELAY[7],False)       # setting Relay 8(RELAY[7]  False if Input 1 is low
-        print "sw off"
+       GPIO.output(RELAY[relay_no],True)
+
+#"""********************************************************************""""
+
+
+#"""*********************************************************************"""
+#Function Name   :Turn_ON_Relay(relay_no)
+#Descriptions    :Turns ON corresponding Relays. Relay Pin goes high
+#Argument        :relay_no (Relay Number between 1 to 8)
+#Return          :NA
+"***********************************************************************"""
+
+def Turn_OFF_Relay(relay_no):
+    relay_no=relay_no-1
+    if((relay_no>7) or (relay_no<0)):
+        print("please rnter correct relay noumber between 1 to 8")
+    else:
+       GPIO.output(RELAY[relay_no],False)
+       
+#"""********************************************************************""""
+
+
+#"""*********************************************************************"""
+#Function Name   :LED_ON
+#Descriptions    :LED Turns ON, Led pin goes high
+#Argument        :none
+#Return          :none
+"***********************************************************************"""
+def LED_ON():
+    GPIO.output(LED[0],True)
+
+#"""********************************************************************""""
+
+#"""*********************************************************************"""
+#Function Name   :LED_OFF
+#Descriptions    :LED Turns OFF, Led pin goes Low
+#Argument        :none
+#Return          :none
+"***********************************************************************"""
+
+def LED_OFF():
+    GPIO.output(LED[0],False)
     
-    #-- samilarly all th IOs can be used based on application-----#
+#"""********************************************************************""""
+
+#"""*********************************************************************"""
+#Function Name   :Read_Input(input_no)
+#Descriptions    :Reads corresponding input ie high or low
+#Argument        :input_no (Input Number to be read between 1 to 8)
+#Return          :none
+"***********************************************************************"""
+
+def Read_Input(input_no):
+    input_no=input_no-1
+    if((input_no>7) or (input_no<0)):
+        print("please rnter correct input noumber between 1 to 8")
+        return 
+    if(GPIO.input(INPUT[input_no])==0):
+       input_data=0
+    else:
+       input_data=1
+    return input_data
+#"""********************************************************************""""
+
+while(1):
+    LED_ON()
+    Turn_ON_Buzzer()
+    sleep(0.5)
+    Turn_OFF_Buzzer()
+    LED_OFF()
+    if(0==Read_Input(3)):
+            print "sw ON"
+            Turn_ON_Relay(3)
+     else:
+            print  "sw off"
+            Turn_OFF_Relay(3)
+    
+
